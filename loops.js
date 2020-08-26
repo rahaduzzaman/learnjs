892e7a9c37935b2c0e460b7e1240fc976c130686
rahaduzzaman <rahad1@ymail.com>
2020-08-26 00:31:50 -0400

    Loops

--- loops.js
+++ loops.js
@@ -12,7 +12,7 @@ for (var i = 0; i < arr.length; i++) {
 } */
 
 // Practice 1:
-function multiplyAll(arr) {
+/* function multiplyAll(arr) {
     var product = 1;
 
     for (var i = 0; i < arr.length; i++) {
@@ -25,4 +25,68 @@ function multiplyAll(arr) {
     return product;
 }
 
-multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]); */
+
+// Example 2 - Recursion
+/* function multiply(arr, n) {
+    if (n <= 0) {
+        return 1;
+    } else {
+        return multiply(arr, n - 1) * arr[n - 1];
+    }
+}
+
+console.log(multiply([2, 3, 4, 5], 3)); */
+
+// FreecodeCamp loop exercise - PROFILE LOOKUP
+
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop) {
+    // My Solution -
+    /* for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            if (contacts[i].hasOwnProperty(prop)) {
+                return contacts[i][prop];
+            } else {
+                return "No such property";
+            }
+        }
+    }
+    return "No such contact"; */
+    // FreeCodeCamp Solution -
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            return contacts[i][prop] || "No such property";
+        }
+    }
+    return "No such contact";
+}
+
+console.log(lookUpProfile("Bob", "potato"));
